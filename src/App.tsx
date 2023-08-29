882b8433b25c2f66b875c7de27de20109768b059
Patrick Caldwell <tncbbthositg@gmail.com>
2023-08-29 13:52:25 -0400

    patrick | adding a color alias for a commonly used color

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,23 @@ function App() {
   return (
     <div className="container bg-white rounded-lg my-16 p-8 mx-auto">
       <form className="grid grid-cols-2 gap-x-4 gap-y-4">
-        <h1 className="text-teal-600 font-medium text-3xl col-span-full">Contact Us</h1>
+        <h1 className="text-primary font-medium text-3xl col-span-full">Contact Us</h1>
         
-        <input type="text" placeholder="First Name" className="text-teal-600 border-b border-teal-600 text-xl py-2" />
-        <input type="text" placeholder="Last Name" className="text-teal-600 border-b border-teal-600 text-xl py-2" />
+        <input type="text" placeholder="First Name" className="text-primary border-b border-primary text-xl py-2" />
+        <input type="text" placeholder="Last Name" className="text-primary border-b border-primary text-xl py-2" />
 
         <div className="mt-8 col-span-full">
-          <input type="text" placeholder="Phone Number" className="w-full text-teal-600 border-b border-teal-600 text-xl py-2" />
+          <input type="text" placeholder="Phone Number" className="w-full text-primary border-b border-primary text-xl py-2" />
         </div>
         <div className="col-span-full">
-          <input type="text" placeholder="Email Address" className="w-full text-teal-600 border-b border-teal-600 text-xl py-2" />
+          <input type="text" placeholder="Email Address" className="w-full text-primary border-b border-primary text-xl py-2" />
         </div>
         
-        <h3 className="text-teal-600 font-medium text-xl mt-8 col-span-full">Message:</h3>
-        <textarea className="col-span-full h-80 border border-teal-600" />
+        <h3 className="text-primary font-medium text-xl mt-8 col-span-full">Message:</h3>
+        <textarea className="col-span-full h-80 border border-primary" />
 
         <div className="flex col-span-full justify-end mt-8">
-          <button type="submit" className="bg-teal-600 border border-teal-600 px-8 py-2 rounded text-white text-lg">Send</button>
+          <button type="submit" className="bg-primary border border-primary px-8 py-2 rounded text-white text-lg">Send</button>
         </div>
       </form>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,6 +8,10 @@ export default {
   ],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      primary: colors.teal[600],
+    }
   },
   plugins: [],
 }
