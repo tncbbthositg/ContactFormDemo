94d8184f77c015ed68d948f59f5d1ef5a9879809
Patrick Caldwell <tncbbthositg@gmail.com>
2023-08-29 15:43:01 -0400

    patrick | allowing emails to be sent with emailjs

--- src/hooks/useSendEmail.ts
+++ src/hooks/useSendEmail.ts
@@ -1,39 +1,54 @@
 import { useCallback, useState } from "react";
+import emailjs from "@emailjs/browser";
 import { ContactInfo } from "../models";
 
 type SendFunction = (info: ContactInfo, emailTemplate: string ) => Promise<void>;
 
 type UseSendEmailResult = {
-    isSending: boolean;
-    sendEmail: SendFunction;
+  isSending: boolean;
+  sendEmail: SendFunction;
 };
 
-const requestbinUrl = import.meta.env.VITE_REQUESTBIN_URL;
+const { VITE_REQUESTBIN_URL, VITE_EMAILJS_SERVICE_ID, VITE_EMAILJS_PUBLIC_KEY } = import.meta.env;
 
-export function useSendEmail(): UseSendEmailResult {
+const sendEmailJs: SendFunction = async (info, emailTemplate) => {
+  if (!VITE_EMAILJS_SERVICE_ID || !VITE_EMAILJS_PUBLIC_KEY) { return; }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  await emailjs.send(VITE_EMAILJS_SERVICE_ID, emailTemplate, info as any, VITE_EMAILJS_PUBLIC_KEY);
+}
+
+const fakeEmail: SendFunction = async (info, emailTemplate) => {
+  if (!VITE_REQUESTBIN_URL) { return; }
+
+  await fetch(
+    `${VITE_REQUESTBIN_URL}/${emailTemplate}`,
+    {
+      method: 'POST',
+      body: JSON.stringify(info),
+    }
+  );
+};
+
+  export function useSendEmail(): UseSendEmailResult {
     const [isSending, setIsSending] = useState(false);
 
     const sendEmail: SendFunction = useCallback(
-        async (info, emailTemplate) => {
-            setIsSending(true);
+      async (info, emailTemplate) => {
+        setIsSending(true);
 
-            try {
-              await fetch(
-                `${requestbinUrl}/${emailTemplate}`,
-                {
-                  method: 'POST',
-                  body: JSON.stringify(info),
-                }
-              );
-            } catch(ex) {
-              // TODO: handle send errors rather than ignoring them
-              console.log("WE SHOULD LOG THIS SOMEWHERE", ex);
-            }
+        try {
+          sendEmailJs(info, emailTemplate);
+          fakeEmail(info, emailTemplate);
+        } catch(ex) {
+          // TODO: handle send errors rather than ignoring them
+          console.log("WE SHOULD LOG THIS SOMEWHERE", ex);
+        }
 
-            setIsSending(false);
-        },
-        []
-    );
+        setIsSending(false);
+      },
+      []
+      );
 
-    return { isSending, sendEmail };
-}
+      return { isSending, sendEmail };
+    }
