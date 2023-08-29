f36018bac8f0d4d33c61f00fad85262845d54244
Patrick Caldwell <tncbbthositg@gmail.com>
2023-08-29 14:35:23 -0400

    patrick | slightly less basic validation

--- src/models/ContactInfo.ts
+++ src/models/ContactInfo.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
 export const ContactInfo = z.object({
-  firstName: z.string().nonempty(),
-  lastName: z.string().nonempty(),
-  emailAddress: z.string().nonempty(),
-  phoneNumber: z.string().nonempty(),
+  firstName: z.string().nonempty("Please enter your first name"),
+  lastName: z.string().nonempty("Please enter your last name"),
+  emailAddress: z.string()
+    .nonempty("Please enter a contact email address")
+    .email("Please enter a valid email address"),
+  phoneNumber: z.string().nonempty("Please enter a contact phone number"),
   message: z.string().optional(),
 });
 
